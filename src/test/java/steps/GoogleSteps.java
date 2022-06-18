b6d9246f3adb202a90606ef67a23777fda5fdf4b
“ramiturkmani” <ramiturkmani1@gmail.com>
2022-06-18 15:49:34 -0500

    initial commit

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,46 @@
+package steps;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import pages.GoogleSearchPage;
+import utils.Driver;
+
+public class GoogleSteps {
+
+    WebDriver driver;
+    GoogleSearchPage googleSearchPage;
+
+    @Before
+    public void setup(){
+        driver = Driver.getDriver();
+        googleSearchPage = new GoogleSearchPage();
+    }
+
+    //We will be writing out actual script steps here
+    @When("user searches for {string} on Google")
+    public void user_searches_for_on_google(String key) {
+        //driver.findElement(By.name("q")).sendKeys(key + Keys.ENTER);
+        googleSearchPage.searchInputBox.sendKeys(key + Keys.ENTER);
+    }
+
+    @Then("user should see {string} in the url")
+    public void user_should_see_in_the_url(String key) {
+        Assert.assertTrue(driver.getCurrentUrl().contains(key));
+    }
+
+    @Then("user should see {string} in the title")
+    public void user_should_see_in_the_title(String key) {
+        Assert.assertTrue(driver.getTitle().contains(key));
+    }
+
+    @Then("user should see results are more than {long}")
+    public void userShouldSeeResultsAreMoreThan(long result) {
+        String resultsText = googleSearchPage.resultBar.getText();
+        Assert.assertTrue(Long.parseLong(resultsText.substring(0, resultsText.indexOf("results")).replaceAll("[\\D]", "")) > 0);
+    }
+}
